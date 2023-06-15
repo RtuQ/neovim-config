18f03434df33243a937429a76cc0cdb62ed54354
Howe <1017264892@qq.com>
2023-06-16 00:37:39 +0800

    :sparkles: Change keymap for bufferline
    Change keymaps for BufferLine

--- lua/howe/core/keymaps.lua
+++ lua/howe/core/keymaps.lua
@@ -26,10 +26,11 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tc", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+-- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+-- bufferline
+keymap.set("n", "<leader>tc", ":BufferLinePickClose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":BufferLineCycleNext<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":BufferLineCyclePrev<CR>") --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds
